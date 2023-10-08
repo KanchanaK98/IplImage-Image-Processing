cb765a944f1b8a7483c765f09b41d4cb495af512
KanchanaK98 <kanchanakariyawasam98@gmail.com>
2023-10-08 19:37:35 +0530

    added histograms for original and contrasted images

--- Lab1.cpp
+++ Lab1.cpp
@@ -44,15 +44,78 @@ int main(int argc, char* argv[])
 		}
 	}
 
+	//gray image histogram
+	int hist[256];
+	for (int i = 0; i < 256; i++) {
+		hist[i] = 0;
+	}
+
+	for (int i = 0; i < h; i++) {
+		for (int j = 0; j < w; j++) {
+			t1 = gr.at<uchar>(i, j);
+			hist[t1] = hist[t1] + 1;
+		}
+	}
+	int max = 0;
+	for (int i = 0; i < 256; i++) {
+		//cout << "Gray Level-" << i << " = " << hist[i] << endl;
+		if (max < hist[i])
+			max = hist[i];
+	}
+	Mat him(601, 520, CV_8UC1, Scalar(255));
+	int his[256];
+	double maxd = max;
+	for (int i = 0; i <= 255; i++) {
+		his[i] = cvRound(double(hist[i] / maxd) * 600);
+		Point pt1 = Point(2 * i, 600 - his[i]);
+		Point pt2 = Point(2 * i, 600);
+		line(him, pt1, pt2, Scalar(0), 1, 8, 0);
+	}
+
+	//contrasted image histogram
+	int hist2[256];
+	for (int i = 0; i < 256; i++) {
+		hist2[i] = 0;
+	}
+
+	for (int i = 0; i < h; i++) {
+		for (int j = 0; j < w; j++) {
+			t1 = cst.at<uchar>(i, j);
+			hist2[t1] = hist2[t1] + 1;
+		}
+	}
+	max = 0;
+	for (int i = 0; i < 256; i++) {
+		//cout << "Gray Level-" << i << " = " << hist[i] << endl;
+		if (max < hist2[i])
+			max = hist2[i];
+	}
+	Mat him2(601, 520, CV_8UC1, Scalar(255));
+	int his2[256];
+	maxd = max;
+	for (int i = 0; i <= 255; i++) {
+		his2[i] = cvRound(double(hist2[i] / maxd) * 600);
+		Point pt1 = Point(2 * i, 600 - his2[i]);
+		Point pt2 = Point(2 * i, 600);
+		line(him2, pt1, pt2, Scalar(0), 1, 8, 0);
+	}
+
+
 	cout << "Width and Height :" << w << ", " << h << endl;
 	imwrite("cst.jpg", cst);
 	namedWindow("image", WINDOW_NORMAL);
 	namedWindow("High_Contrast", WINDOW_AUTOSIZE);
+	namedWindow("Original Histogram", WINDOW_AUTOSIZE);
+	namedWindow("Contrasted Histogram", WINDOW_AUTOSIZE);
 	imshow("image", gr);
 	imshow("High_Contrast", cst);
+	imshow("Original Histogram", him);
+	imshow("Contrasted Histogram", him2);
 	waitKey(0);
 	img.release();
 	gr.release();
 	cst.release();
+	him.release();
+	him2.release();
 	return 0;
 }
